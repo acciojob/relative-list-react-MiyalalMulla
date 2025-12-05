58345ca086ad6ceb0108786a0aa6faf552d494b2
MiyalalMulla <mullamiyalal@gmail.com>
2025-12-05 11:54:48 +0530

    completed relative

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import RelativeList from "./RelativeList";
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <RelativeList />
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
